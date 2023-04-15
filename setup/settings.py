ffeb959f275568e6e2dea9be404ef70a4b2e06d7
Pedro Dias <pedroltdias@gmail.com>
2023-04-15 17:44:33 -0300

    env: enabling CORS

--- setup/settings.py
+++ setup/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'rest_framework',
     'empresa',
-    'django_filters'
+    'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 25,
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning'
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
