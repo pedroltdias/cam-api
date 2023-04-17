9a530e972962fdec36a837d0f440d6c8befc6285
Pedro Dias <pedroltdias@gmail.com>
2023-04-17 10:38:07 -0300

    test: testing http methods get and post

--- empresa/test/testes_departamentos.py
+++ empresa/test/testes_departamentos.py
@@ -1,6 +1,7 @@
 from rest_framework.test import APITestCase
 from empresa.models import Departamento
 from django.urls import reverse
+from rest_framework import status
 
 class DepartamentoTestCase(APITestCase):
     
@@ -14,3 +15,16 @@ class DepartamentoTestCase(APITestCase):
 			nome="DepartamentoTesteDois"
 		)
 
+	def test_requisicao_get_para_listar_departamentos(self):
+		"""Teste para verificar a requisicao GET para listar departamentos"""
+		response = self.client.get(self.list_url)
+		self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+	def test_requisicao_post_para_criar_departamento(self):
+		"""Teste para verificar a requisicao POST para criar um departamento"""
+		data = {
+			'nome': 'DepartamentoTeste'
+		}
+
+		response = self.client.post(self.list_url, data=data)
+		self.assertEquals(response.status_code, status.HTTP_201_CREATED)
